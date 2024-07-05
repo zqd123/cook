833df88d17dc721548c4f3c10e7d44d09f13ddf1
zhangqd <zhangqd@digiwin.com>
2024-07-05 16:45:19 +0800

    feat: 更改cron

--- start.js
+++ start.js
@@ -13,25 +13,19 @@ let cookieGlobal = "";
 let eventGlobal = "";
 
 /** 定时登陆 */
-schedule.scheduleJob("0 59 15 * * ?", async () => {
+schedule.scheduleJob("0 59 15 * * 2,4", async () => {
   // 登录
   const loginRes = await login(userConfig);
   cookieGlobal = loginRes.headers["set-cookie"][0].split(";")[0];
-  console.log("登陆成功！！！", "用户cookie：", cookieGlobal);
+  console.log("登陆成功！！！");
 });
 
 /** 下单 */
-schedule.scheduleJob("0 0 16 * * ?", async () => {
+schedule.scheduleJob("0 0 16 * * 2,4", async () => {
   // 获取eventId
   const eventRes = await getEventId({ cookie: cookieGlobal });
   eventGlobal = eventRes.data.data[0].eventId;
-  console.log(
-    "获取eventId成功！！！",
-    "eventId:",
-    eventGlobal,
-    ";状态：",
-    eventRes?.data?.data[0]?.status
-  );
+  console.log("获取eventId成功！！！");
   // 点单
   for (const item of submitMenus) {
     const submitRes = await submitCook({
